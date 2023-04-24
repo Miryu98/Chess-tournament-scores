d9c0a5f72733627bced2637e1ee13dbac6cdfbef
Miłosz Salwa <75585351+Miryu98@users.noreply.github.com>
2023-04-24 14:23:48 +0200

    Update Chess.java

--- Chess.java
+++ Chess.java
@@ -3,14 +3,15 @@ public class Chess {
         int wins = 54;
         int ties = 31;
 
-        double multiply_points = wins * 1.0 + ties * 0.5;
+        double points = wins * 1.0 + ties * 0.5;
 
-        System.out.println(multiply_points);
+        System.out.println("Points earned by the player: " + points);
     }
 }
 
 
-"You are making a program for a chess tournament, that needs to calculate the points earned by a player.
+/* You are making a program for a chess tournament, that needs to calculate the points earned by a player.
 A win is worth 1 point, while a tie is worth 0.5 points.
 The given program declares two variables: wins and ties with the corresponding values.
 Create a program to calculate and output the points earned by the player."
+*/
