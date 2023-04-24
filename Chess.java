bd84c41b3ac388237b80585aa2849e4ddfd4f5b3
Miłosz Salwa <75585351+Miryu98@users.noreply.github.com>
2023-04-24 14:19:54 +0200

    Create Chess.java

---
+++ Chess.java
@@ -0,0 +1,16 @@
+public class Chess {
+    public static void main(String[] args) {
+        int wins = 54;
+        int ties = 31;
+
+        double multiply_points = wins * 1.0 + ties * 0.5;
+
+        System.out.println(multiply_points);
+    }
+}
+
+
+"You are making a program for a chess tournament, that needs to calculate the points earned by a player.
+A win is worth 1 point, while a tie is worth 0.5 points.
+The given program declares two variables: wins and ties with the corresponding values.
+Create a program to calculate and output the points earned by the player."
